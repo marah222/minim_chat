d50b9d7fd59a4e832fb6d74d90d24794616f7cce
MarooHsn222 <marooram222@gmail.com>
2025-02-12 10:21:54 +0100

    menu drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_chat/auth/auth_service.dart';
 
+import '../components/my_drawer.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() {
-    final _auth = AuthService();
-    _auth.singOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
-        actions: [
-          // logout button
-          IconButton(onPressed: logout, icon: Icon(Icons.logout))
-        ],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
